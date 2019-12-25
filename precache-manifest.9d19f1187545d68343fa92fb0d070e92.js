aef2d585172d5d6d0ee5bbeacbc38880f8c1274d
Pavel <pavel-998@mail.ru>
2019-12-25 23:58:02 +0700

    Updates

---
+++ precache-manifest.9d19f1187545d68343fa92fb0d070e92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0e3fd8241978928979759f11ed688d",
+    "url": "/mvc-rgr/index.html"
+  },
+  {
+    "revision": "f761cb6a4e4b79222562",
+    "url": "/mvc-rgr/static/css/main.77c405a0.chunk.css"
+  },
+  {
+    "revision": "4683901f2de34999b7ec",
+    "url": "/mvc-rgr/static/js/2.49138942.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/mvc-rgr/static/js/2.49138942.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f761cb6a4e4b79222562",
+    "url": "/mvc-rgr/static/js/main.1056e09d.chunk.js"
+  },
+  {
+    "revision": "6178918c58c5f48f8a1a",
+    "url": "/mvc-rgr/static/js/runtime-main.9e75151a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mvc-rgr/static/media/logo.5d5d9eef.svg"
+  }
+]);
